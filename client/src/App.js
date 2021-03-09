637486b42ff4172a8453de50c4e1502850b6eea4
lojithv <lojithvinsuka1998@gmail.com>
2021-03-09 13:00:20 +0530

    Mainmenu

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Home from './components/Home/Home';
+import Mainmenu from './components/Mainmenu/Sidemenu';
 import Navbar from './components/Navbar/Navbar';
 import Auth from './components/Auth/Auth';
 
@@ -11,7 +11,7 @@ const App = () => (
     <Container maxWidth="lg">
       <Navbar />
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={Mainmenu} />
         <Route path="/auth" exact component={Auth} />
       </Switch>
     </Container>
