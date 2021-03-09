36e09e59f6526a2a3f310c344de873d4e728c96e
lojithv <lojithvinsuka1998@gmail.com>
2021-03-09 14:06:47 +0530

    Side menu material ui

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,15 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import { reducers } from './reducers';
-import App from './App';
+//import App from './App';
+import NavBar from './components/NavigationDrawer/NavDrawer';
 import './index.css';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <NavBar />
   </Provider>,
   document.getElementById('root'),
 );
