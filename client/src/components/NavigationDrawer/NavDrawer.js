867cf27f6face512e41e1a27c1c0ae6cda4bcf9d
senith2001 <senithdeelaka10@gmail.com>
2021-04-21 20:40:45 +0530

    Edit demo

--- client/src/components/NavigationDrawer/NavDrawer.js
+++ client/src/components/NavigationDrawer/NavDrawer.js
@@ -86,8 +86,8 @@ export default function MiniDrawer() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap>
-            Mini variant drawer
+          <Typography variant="h5" noWrap>
+            LOGO
           </Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
@@ -109,6 +109,7 @@ export default function MiniDrawer() {
                 <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
                 <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
                 <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+                <Button component={Link} to="/auth" variant="contained" color="primary">Upload</Button>
               </div>
             ) : (
               <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
